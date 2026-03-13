9def7cba178c415a498dde6e18657ca34e6fd3df
sjfuentes-uniandes <sj.fuentes@uniandes.edu.co>
2026-03-13 00:24:51 -0500

    pagina configuracion

--- src/app/pages/configuracion/configuracion.component.ts
+++ src/app/pages/configuracion/configuracion.component.ts
@@ -3,20 +3,37 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-configuracion',
   standalone: true,
-  imports: [MatTabsModule, MatFormFieldModule, MatInputModule],
+  imports: [MatTabsModule, MatFormFieldModule, MatInputModule, CommonModule],
   templateUrl: './configuracion.component.html',
   styleUrl: './configuracion.component.scss'
 })
 export class ConfiguracionComponent {
+  periods = [
+    { startPeriod: 'AM', endPeriod: 'AM' },
+    { startPeriod: 'AM', endPeriod: 'AM' },
+    { startPeriod: 'AM', endPeriod: 'AM' }
+  ];
+
   constructor(private router: Router) {}
 
   onTabChange(index: number) {
     if (index === 0) {
       this.router.navigate(['/dashboard']);
+    } else if (index === 2) {
+      // Navegación a favoritos cuando esté implementado
+    }
+  }
+
+  togglePeriod(rowIndex: number, isStart: boolean) {
+    if (isStart) {
+      this.periods[rowIndex].startPeriod = this.periods[rowIndex].startPeriod === 'AM' ? 'PM' : 'AM';
+    } else {
+      this.periods[rowIndex].endPeriod = this.periods[rowIndex].endPeriod === 'AM' ? 'PM' : 'AM';
     }
   }
 }
